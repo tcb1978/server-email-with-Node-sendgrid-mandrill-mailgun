6c51890562ab6defc7955ac015955ca2298369f8
Matthew Eldredge <mattheweldredge@gmail.com>
2018-04-24 18:14:38 -0700

    set up node server

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var router = express.Router();
-
+const express = require('express');
+const router = express.Router();
+const sendgrid = require('sendgrid')(process.env.USER, process.env.PASS);
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  sendgrid.send({
+    to: process.env.FROM,
+    from: process.env.TO,
+    text: 'This is an Email through sendgrid'
+  }, (err, json) => {
+    if (err) {
+      return console.error(err);
+    }
+    console.log(json );
+  })
+  // res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
